cc01e7d516f76e890af5d2ecca9282505427bde7
Luka <luka.dev94@gmail.com>
2019-11-18 10:42:12 +0100

    World Interactions

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    public int MaxHealth = 5;
+    public float Speed = 3.0f;
+    public int Health { get { return this.currentHealth; } }
+
+    int currentHealth;
+    Rigidbody2D rigidbody2d;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + Speed * horizontal * Time.deltaTime;
+        position.y = position.y + Speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, MaxHealth);
+        Debug.Log(currentHealth + "/" + MaxHealth);
+    }
+}
